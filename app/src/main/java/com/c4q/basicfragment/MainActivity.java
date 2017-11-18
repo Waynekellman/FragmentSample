346ebde3c974b0420b0fc1e0b4b67513d50082ef
lighterletter <jgomez818@gmail.com>
2017-11-18 11:50:19 -0500

    adding toggle

--- app/src/main/java/com/c4q/basicfragment/MainActivity.java
+++ app/src/main/java/com/c4q/basicfragment/MainActivity.java
@@ -8,21 +8,32 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    boolean isFragmentShown = false;
+    FragmentManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.button1).setOnClickListener(this);
+        manager = getFragmentManager();
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.button1){
-            //initialize a fragment on start-up
-            FragmentManager manager = getFragmentManager();
+        if (view.getId() == R.id.button1) {
             FragmentTransaction transaction = manager.beginTransaction();
-            transaction.replace(R.id.fagment_container, new BaseFragment());
-            transaction.commit();
+
+            if (!isFragmentShown) {
+                //initialize a fragment on start-up
+                transaction.replace(R.id.fagment_container, new BaseFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+                isFragmentShown = true;
+            } else {
+                isFragmentShown = false;
+                onBackPressed();
+            }
         }
     }
 }
